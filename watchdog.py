c05f668ebab068c9908180ccfb08143795e40c63
phillip1011 <99014094+phillip1011@users.noreply.github.com>
2022-02-04 17:57:10 +0800

    test1
    modify  serverip

--- watchdog.py
+++ watchdog.py
@@ -18,7 +18,8 @@ def getmac():
 
 
 def getlocalip():
-    serverip= "35.221.198.141"  #hhremote.com主機
+    # serverip= "35.221.198.141"  #hhremote.com主機
+    serverip =  "127.0.0.1"
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         s.connect((serverip, 80))
