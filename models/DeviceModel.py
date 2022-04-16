3406bf1154de26840c5a2048e575c32c8e077edb
solitude12089 <solitude12089@gmail.com>
2022-04-16 13:10:21 +0800

    修改 locaip 抓取邏輯

--- models/DeviceModel.py
+++ models/DeviceModel.py
@@ -14,37 +14,32 @@ class DeviceModel:
   
     
     def __init__(self):
+        cf = configparser.ConfigParser()
+        cf.read("/home/ubuntu/hhinfo_PI/config.ini")
+        self.localport = cf.get("DeviceConfig", "defaultport")
+        serverip =  cf.get("ServerConfig", "serverip")
+        forceLocalIp =  cf.get("DeviceConfig", "forceLocalIp")
+        if forceLocalIp == 'true' :
+            self.localip = cf.get("DeviceConfig", "defaultip")
+        else :
+            self.localip = self.getLocalipByServer(serverip)
+
         conn = sqlite3.connect("/home/ubuntu/hhinfo_PI/cardno.db")
         cur = conn.cursor()
         cur.execute('select * from device')
         data = cur.fetchone()
         conn.close()
-
-
-        cf = configparser.ConfigParser()
-        cf.read("/home/ubuntu/hhinfo_PI/config.ini")
-        self.localport = cf.get("DeviceConfig", "defaultport")
-        serverip =  cf.get("ServerConfig", "serverip")
-
-        if data == None:
-            forceLocalIp =  cf.get("DeviceConfig", "forceLocalIp")
-            if forceLocalIp == 'true' :
-                self.localip = cf.get("DeviceConfig", "defaultip")
-            else :
-                self.localip = self.getLocalipByServer(serverip)
-        else :
+        if data != None:
             self.name = data[5]
             self.family = data[6]
             self.mode = data[8]
             self.doortype = data[10]
             self.localip = data[1].split(':',1)[0]
             self.localport = data[1].split(':',1)[1]
-
         if self.doortype=='一般':
             self.opendoortime =(int)(cf.get("DeviceConfig", "opendoortime_normal"))
         else:
             self.opendoortime =(int)(cf.get("DeviceConfig", "opendoortime_iron"))
-       
         self.show()
 
 
@@ -58,8 +53,7 @@ class DeviceModel:
         print("localport = " + self.localport)
         print("opendoortime = " , self.opendoortime)
 
-    def getLocalipByServer(self,serverip):
-
+    def getLocalipByServer(self):
         # 先從tun0 , 抓取ip
         cmd = "/sbin/ip -o -4 addr list tun0 | awk '{print $4}' | cut -d/ -f1"
         output = subprocess.getoutput(cmd)
