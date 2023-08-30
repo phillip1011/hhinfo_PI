591831290df636e2c1c784e96bbfb46cf9bcdae1
phillip1011 <phillip@hhinfo.com.tw>
2023-08-30 17:26:40 +0800

    fix bug  for schedule

--- schedule.py
+++ schedule.py
@@ -10,6 +10,25 @@ controlip=""
 poweredByTime=""
 spcard_auth_ac="No"
 
+
+def chk_sp_auth():
+    #print("檢查是否有全區卡佔用")
+    conn=sqlite3.connect("/home/ubuntu/hhinfo_PI/cardno.db")
+    c=conn.cursor()
+    c.execute(
+    'select * from spcard_time where end_time>?', 
+    (datetime.now(),)
+    )
+    spcard_auth = c.fetchone()
+    conn.close()
+
+    if spcard_auth != None:
+        #print("全區卡時段佔用中")
+        return 1
+    else:
+        #print("無全區卡佔用中")
+        return 0
+
 def spcard_Ry(relays,spcard_auth_ac):
     relays=relays.split(',')
     for relay in relays:
@@ -54,9 +73,8 @@ def chkRyStatus():
             timerange['_range_id']
         )
     )
+ 
     data = c.fetchone()
-
-
     if data == None:
         print("本時段無預約,檢查是否有延後關燈時間")
         conn=sqlite3.connect("/home/ubuntu/hhinfo_PI/cardno.db")
@@ -74,28 +92,10 @@ def chkRyStatus():
         )
         data = c.fetchone()
         conn.close()
-#----------------------------------------------------------------          
-        #print("檢查是否有全區卡佔用")
-        conn=sqlite3.connect("/home/ubuntu/hhinfo_PI/cardno.db")
-        c=conn.cursor()
-        c.execute(
-        'select * from spcard_time where end_time>?', 
-        (datetime.now(),)
-        )
-        spcard_auth = c.fetchone()
-        conn.close()
-
-        if spcard_auth != None:
-            #print("全區卡時段佔用中")
-            sp_auth = 1
-        else:
-            #print("無全區卡佔用中")
-            sp_auth = 0
-#----------------------------------------------------------------  
 
         if data != None:
             #延後退場時段
-            if sp_auth==1:
+            if chk_sp_auth()==1:
                 print("延後退場時段內, 全區卡佔用中")
                 if spcard_auth != None:
                     spcard_Ry(spcard_auth[3],spcard_auth_ac)
@@ -104,7 +104,7 @@ def chkRyStatus():
                 globals._relay.action(4,0,0)
         else:
             #非延後退場時段
-            if sp_auth==1:
+            if chk_sp_auth()==1:
                 print("非延後退場時段,全區卡佔用中")
                 if spcard_auth != None:
                     spcard_Ry(spcard_auth[3],spcard_auth_ac)
@@ -116,16 +116,16 @@ def chkRyStatus():
     
     else:
         #有預約記錄
-        if poweredByTime == "true":
+        if poweredByTime == "true":   #時間到才送電, 如果是刷卡送電模式, 電燈是刷卡時再送電
             print("本時段有預約:開啟R3並檢查是否開啟R4")
             globals._relay.action(3,255,0)
             if data[4] == '1':
                 print("有預約冷氣:開啟R4")
                 globals._relay.action(4,255,0)
-    if data[4] == '0':  #不管是時間送電還是刷卡送電,只要沒預約都斷冷氣電源
-        if sp_auth==0:
-            print("無預約冷氣:關閉R4")
-            globals._relay.action(4,0,0)  
+        if chk_sp_auth()==0:
+            print ("無全區卡佔用")
+            if data[4] == '0':  #不管是時間送電還是刷卡送電,只要沒預約都斷冷氣電源
+                globals._relay.action(4,0,0)  
 
     conn.commit()
     conn.close()
